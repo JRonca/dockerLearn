b0cc40bbc0da1b1f30150173936f7c38a108c308
JRonca <jrfr21112000@gmail.com>
2025-08-12 21:24:40 -0300

    feat: CMD, Entrypoint and Labels

--- fullCycle/docker_in_practice/mygo_app/main.go
+++ fullCycle/docker_in_practice/mygo_app/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func main() {
+	args := os.Args[1:]
+
+	if len(args) > 0 {
+		fmt.Printf("Starting server on port :%s\n", args[0])
+		http.ListenAndServe(":"+args[0], nil)
+		return
+	}
+
 	fmt.Println("Starting server on :8080")
 	http.ListenAndServe(":8080", nil)
 }
