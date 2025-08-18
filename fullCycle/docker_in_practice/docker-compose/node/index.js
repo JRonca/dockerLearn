0a277939fa029186d36d41a66fb7f53500e175a7
JRonca <jrfr21112000@gmail.com>
2025-08-18 13:08:53 -0300

    feat: environment vars and env file

--- fullCycle/docker_in_practice/docker-compose/node/index.js
+++ fullCycle/docker_in_practice/docker-compose/node/index.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect("mongodb://mongo:27017/test")
+  .connect(`mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/test`)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+console.log(process.env.PORT);
